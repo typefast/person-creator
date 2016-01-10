89946a9f3c09b4ccadecbf226b51e9e5bf237e7c
Harry Oliver <someonecancode@yahoo.co.uk>
2016-01-10 15:27:02 +0000

    Add new action and view

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,4 +1,14 @@
 class PeopleController < ApplicationController
   def index
+    @people = Person.all
   end
+  
+  def new
+    @person = Person.new
+  end
+  
+  private
+    def person_params
+      params.require(:person).permit(:first_name, :last_name)
+    end
 end
